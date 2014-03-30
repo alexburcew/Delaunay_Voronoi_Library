f3704d544f074f1541a4170a633b6e9cb54486be
Dmitry A. Grechka <dmitry.grechka@gmail.com>
2014-03-31 00:14:12 +0400

    Test project with one NNI test

---
+++ Tests/NNI.fs
@@ -0,0 +1,23 @@
+﻿namespace UnitTestProject1
+
+open System
+open Microsoft.VisualStudio.TestTools.UnitTesting
+
+open Delaunay_Voronoi_Library
+
+[<TestClass>]
+type UnitTest() = 
+    [<TestMethod>]
+    member x.TestEqualAreasNNI () = 
+        let vertices = 
+            [|
+                new Vertex(0.0,0.0,3.0);
+                new Vertex(90.0,0.0,6.0);
+                new Vertex(180.0,0.0,9.0);
+                new Vertex(270.0,0.0,12.0);
+                new Vertex(0.0,90.0,15.0);
+                new Vertex(0.0,-90.0,18.0)
+            |]
+        let voronoi = new Delaunay_Voronoi(new Collections.Generic.List<Vertex>(vertices),false);
+        let res = voronoi.NatNearestInterpolation(45.0,45.0,false,false)
+        Assert.AreEqual(8.0, res.Value,1e-12)
